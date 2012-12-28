d5ae94d8617b9e6d32d415c77c50689fa5a1c49e
Franz Liedke <franz@develophp.org>
2012-12-28 14:37:56 +0100

    No more spaghetti code.

--- src/franzliedke/ArrayShortener/ShortenCommand.php
+++ src/franzliedke/ArrayShortener/ShortenCommand.php
@@ -10,6 +10,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class ShortenCommand extends Command
 {
+	protected $balance = 0;
+	protected $array_balancer_stack = array();
+
 	protected function configure()
 	{
 		$this->setName('shorten')
@@ -22,50 +25,98 @@ class ShortenCommand extends Command
 		$file = $input->getArgument('file');
 		$source = file_get_contents($file);
 
-		$tokens = token_get_all($source);
-
-		$balance = 0;
-		$array_balancer_stack = array();
+		$tokens = $this->tokenize($source);
 
 		foreach ($tokens as $token)
 		{
-			if (is_array($token))
-			{
-				$id = $token[0];
-				$token = $token[1];
+			$convertedToken = $this->handleToken($token);
+			$output->write($convertedToken);
+		}
+	}
 
-				if ($id == T_ARRAY)
-				{
-					$array_balancer_stack[] = $balance;
-					$token = '';
-				}
-			}
-			// Handle single characters
-			else
-			{
-				if ($token == '(')
-				{
-					if (end($array_balancer_stack) == $balance)
-					{
-						$token = '[';
-					}
+	protected function tokenize($source)
+	{
+		$tokens = token_get_all($source);
 
-					$balance++;
-				}
-				else if ($token == ')')
-				{
-					$balance--;
+		// Reset our parenthesis balancer
+		$this->balance = 0;
+		$this->array_balancer_stack = array();
 
-					if (end($array_balancer_stack) == $balance)
-					{
-						$token = ']';
-						array_pop($array_balancer_stack);
-					}
-				}
-			}
+		return $tokens;
+	}
 
-			$output->write($token);
+	protected function handleToken($token)
+	{
+		if (is_array($token))
+		{
+			return $this->handleArrayToken($token);
+		}
+		else
+		{
+			return $this->handleStringToken($token);
+		}
+	}
+
+	protected function handleArrayToken($token)
+	{
+		$id = $token[0];
+
+		if ($id == T_ARRAY)
+		{
+			return $this->handleOldArray();
 		}
 
+		// All other tokens can be left untouched
+		return $token[1];
+	}
+
+	protected function handleOldArray()
+	{
+		$this->array_balancer_stack[] = $this->balance;
+
+		// We just return an empty string here, as the actual square brackets
+		// will be inserted when we encounter the parentheses belonging to this
+		// old-style array.
+		return '';
+	}
+
+	protected function handleStringToken($token)
+	{
+		if ($token == '(')
+		{
+			return $this->handleOpeningParen();
+		}
+		else if ($token == ')')
+		{
+			return $this->handleClosingParen();
+		}
+
+		// All other tokens can be left untouched
+		return $token;
+	}
+
+	protected function handleOpeningParen()
+	{
+		if (end($this->array_balancer_stack) == $this->balance)
+		{
+			$this->balance++;
+			return '[';
+		}
+
+		$this->balance++;
+		return '(';
+	}
+
+	protected function handleClosingParen()
+	{
+		$this->balance--;
+
+		if (end($this->array_balancer_stack) == $this->balance)
+		{
+			array_pop($this->array_balancer_stack);
+			return ']';
+		}
+
+		return ')';
 	}
 }
