5d40c0e29a4ebb67bb92be268a3b7ca067d10cb2
Franz Liedke <franz@develophp.org>
2012-12-28 18:28:05 +0100

    Fix array typehints being stripped. Fixes #3.

--- src/franzliedke/ArrayShortener/Shortener.php
+++ src/franzliedke/ArrayShortener/Shortener.php
@@ -7,15 +7,19 @@ class Shortener
 	protected $balance = 0;
 	protected $array_balancer_stack = array();
 
+	protected $tokens = array();
+
 	public function shorten($source)
 	{
-		$tokens = $this->tokenize($source);
+		$this->tokens = $this->tokenize($source);
 
 		$output = '';
-		foreach ($tokens as $token)
+		foreach ($this->tokens as $index => $token)
 		{
-			$output .= $this->handleToken($token);
+			$output .= $this->handleToken($index);
 		}
+
+		$this->tokens = array();
 
 		return $output;
 	}
@@ -31,43 +35,62 @@ class Shortener
 		return $tokens;
 	}
 
-	protected function handleToken($token)
+	protected function handleToken($index)
 	{
-		if (is_array($token))
+		if (is_array($this->tokens[$index]))
 		{
-			return $this->handleArrayToken($token);
+			return $this->handleArrayToken($index);
 		}
 		else
 		{
-			return $this->handleStringToken($token);
+			return $this->handleStringToken($index);
 		}
 	}
 
-	protected function handleArrayToken($token)
+	protected function handleArrayToken($index)
 	{
+		$token = $this->tokens[$index];
 		$id = $token[0];
 
 		if ($id == T_ARRAY)
 		{
-			return $this->handleOldArray();
+			return $this->handleOldArray($index);
 		}
 
 		// All other tokens can be left untouched
 		return $token[1];
 	}
 
-	protected function handleOldArray()
+	protected function handleOldArray($index)
 	{
-		$this->array_balancer_stack[] = $this->balance;
+		if ($this->isOldArrayDefinition($index))
+		{
+			$this->array_balancer_stack[] = $this->balance;
 
-		// We just return an empty string here, as the actual square brackets
-		// will be inserted when we encounter the parentheses belonging to this
-		// old-style array.
-		return '';
+			// We just return an empty string here, as the actual square
+			// brackets will be inserted when we encounter the opening paren
+			// for this old-style array definition statement.
+			return '';
+		}
+
+		return 'array';
 	}
 
-	protected function handleStringToken($token)
+	protected function isOldArrayDefinition($index)
 	{
+		do
+		{
+			$next = $this->tokens[++$index];
+		}
+		while (is_array($next) && $next[0] == T_WHITESPACE);
+
+		return $next == '(';
+	}
+
+	protected function handleStringToken($index)
+	{
+		$token = $this->tokens[$index];
+
 		if ($token == '(')
 		{
 			return $this->handleOpeningParen();
@@ -86,6 +109,7 @@ class Shortener
 		if (count($this->array_balancer_stack) && end($this->array_balancer_stack) == $this->balance)
 		{
 			$this->balance++;
+			
 			return '[';
 		}
 
