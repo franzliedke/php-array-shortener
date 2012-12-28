85c0450fc7ea5fab9fbd20f0f2cad2cfa50bf5f2
Franz Liedke <franz@develophp.org>
2012-12-28 17:46:31 +0100

    Add another, even meaner, test (with whitespace) for array typehints.

---
+++ tests/examples/5/input.php
@@ -0,0 +1,13 @@
+<?php
+
+function some_random_function(array 
+
+
+	$typehint)
+{
+	// Here we just execute random code
+	$var = array   (
+'bar'
+		);
+	return strtolower('BARFOO');
+}
---
+++ tests/examples/5/output.php
@@ -0,0 +1,13 @@
+<?php
+
+function some_random_function(array 
+
+
+	$typehint)
+{
+	// Here we just execute random code
+	$var = [
+'bar'
+		];
+	return strtolower('BARFOO');
+}
