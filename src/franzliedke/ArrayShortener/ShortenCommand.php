bb524aa3fe5bd6ada9af9cf431c689915cc14775
Franz Liedke <franz@develophp.org>
2012-12-28 16:25:27 +0100

    Implement parsing directories.

--- src/franzliedke/ArrayShortener/ShortenCommand.php
+++ src/franzliedke/ArrayShortener/ShortenCommand.php
@@ -2,6 +2,11 @@
 
 namespace franzliedke\ArrayShortener;
 
+use CallbackFilterIterator;
+use DirectoryIterator;
+use EmptyIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -24,16 +29,64 @@ class ShortenCommand extends Command
 	{
 		$this->setName('shorten')
 		     ->setDescription('Convert a PHP file to use the new PHP 5.4+ shorthand array syntax.')
-		     ->addArgument('file', InputArgument::REQUIRED, 'Which file do you want to convert?');
+		     ->addArgument('file', InputArgument::REQUIRED, 'Which file or folder do you want to convert?')
+		     ->addOption('recursive', 'r', InputOption::VALUE_NONE, 'Should subfolders be converted, too?');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$file = $input->getArgument('file');
-		$source = file_get_contents($file);
+		$iterator = $this->getIterator($file, $input);
 
-		$result = $this->shortener->shorten($source);
+		foreach ($iterator as $element)
+		{
+			$output->writeln('Process file '.$element->getPathname());
+			$result = $this->processFile($element->getPathname());
+		}
 
 		$output->write($result);
+	}
+
+	protected function getIterator($file, InputInterface $input)
+	{
+		if (is_dir($file))
+		{
+			// We shall only process PHP files
+			$callback = function($current, $key, $iterator)
+			{
+				return $current->getExtension() == 'php';
+			};
+
+			if ($input->hasOption('recursive'))
+			{
+				$inner = new RecursiveDirectoryIterator($file);
+				$iterator = new RecursiveIteratorIterator($inner, RecursiveIteratorIterator::CHILD_FIRST);
+			}
+			else
+			{
+				$iterator = new DirectoryIterator($file);
+			}
+		}
+		else if (file_exists($file))
+		{
+			// We may only run the single file that was requested.
+			$callback = function ($current, $key, $iterator) use ($file)
+			{
+				return basename($file) == $current->getBasename();
+			};
+
+			$iterator = new DirectoryIterator(dirname($file));
+		}
+		else
+		{
+			return new EmptyIterator;
+		}
+
+		return new CallbackFilterIterator($iterator, $callback);
+	}
+
+	protected function processFile($filename)
+	{
+		return $this->shortener->shorten(file_get_contents($filename));
 	}
 }
